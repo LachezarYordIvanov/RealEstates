1707f1555e92f07516bc9c71b7ebe7c1d43ebc02
Lachezar <lachezarji@gmail.com>
2020-08-26 16:00:14 +0300

    Add migration

---
+++ RealEstates.Data/RealEstateDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Models;
+
+namespace RealEstates.Data
+{
+    public class RealEstateDbContext : DbContext
+    {
+        public RealEstateDbContext()
+        {
+        }
+
+        public RealEstateDbContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<RealEstateProperty> RealEstateProperties { get; set; }
+
+        public DbSet<District> Districts { get; set; }
+
+        public DbSet<BuildingType> BuildingTypes { get; set; }
+
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }  
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=RealEstate;Integrated security=true;");
+            }            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<District>()
+                .HasMany(x => x.Properties)
+                .WithOne(x => x.District)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<RealEstatePropertyTag>()
+                .HasKey(x => new
+                {
+                    x.PropertyId,
+                    x.TagId
+                });
+        }
+    }
+}
