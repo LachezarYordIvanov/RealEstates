408d0ef3c0efd9ceee818a65acec77699adf94de
Lachezar <lachezarji@gmail.com>
2020-09-16 18:21:00 +0300

    Implement GetTopDistrictsByAveragePrice method

--- RealEstates.Services/DistrictsService.cs
+++ RealEstates.Services/DistrictsService.cs
@@ -1,19 +1,44 @@
-﻿using RealEstates.Services.Models;
+﻿using RealEstates.Data;
+using RealEstates.Models;
+using RealEstates.Services.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RealEstates.Services
 {
     public class DistrictsService : IDistrictsService
     {
+        private readonly RealEstateDbContext db;
 
+        public DistrictsService(RealEstateDbContext db)
+        {
+            this.db = db;            
+        }
         public IEnumerable<DistrictViewModel> GetTopDistrictsByAveragePrice(int count = 10)
         {
-            throw new System.NotImplementedException();
+            return db.Districts.OrderByDescending(x => x.Properties.Average(x => x.Price))
+                .Select(x => MapToDistrictViewModel(x))
+                .Take(count)
+                .ToList();
         }
+
+        
 
         public IEnumerable<DistrictViewModel> GetTopDistrictsByNumberOfProperties(int count = 10)
         {
             throw new System.NotImplementedException();
+        }
+
+        private static DistrictViewModel MapToDistrictViewModel(District x)
+        {
+            return new DistrictViewModel
+            {
+                Name = x.Name,
+                AveragePrice = x.Properties.Average(x => x.Price),
+                MinPrice = x.Properties.Min(x => x.Price),
+                MaxPrice = x.Properties.Max(x => x.Price),
+                PropertiesCount = x.Properties.Count(),
+            };
         }
     }
 }
